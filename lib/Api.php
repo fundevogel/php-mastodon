8705e6099575e6ea32cf2d41233a795e2104ade0
Martin Folkers <hello@twobrain.io>
2021-09-17 10:09:11 +0200

    Bump version

--- lib/Api.php
+++ lib/Api.php
@@ -5,7 +5,7 @@
  *
  * @link https://github.com/Fundevogel/php-mastodon
  * @license https://www.gnu.org/licenses/gpl-3.0.txt GPL v3
- * @version 0.3.0
+ * @version 0.4.0
  */
 
 namespace Fundevogel\Mastodon;
